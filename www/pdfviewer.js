70b3196e4fac37bc7660b2aca4150d732ec1fa04
Bois Pierre-Emmanuel <pebois@kernix.com>
2014-01-21 01:04:12 +0100

    Revert 659271e..8fd8dd6
    This rolls back to commit 659271e040f0be8a02c098818a0c406905c9f546.

--- www/pdfviewer.js
+++ www/pdfviewer.js
@@ -6,10 +6,10 @@
 //  MIT Licensed
 
 var PDFViewer = {
-    open: function (url, ext, callback) {
+    open: function (url, title, callback) {
         cordova.exec(callback, function (err) {
-            callback('Error: Preview');
-        }, "PDFViewer", "open", [url, ext]);
+                callback('Error: Preview');
+            }, "PDFViewer", "open", [url, title]);
     }
 };
 
